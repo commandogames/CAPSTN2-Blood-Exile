2e1fb7c0cb4678a5f108d04bd942b4312b49ec32
Johndale Alfred Julian <j.julian@klab.com>
2014-11-30 08:49:59 +0800

    Added feature: Mage Normal Damage Infliction
    - Collidable normal attack particle moving forward
    - Damage computation to GameObjects with tag "Enemy"
    - Initial 3 damage

--- Assets/Scripts/MageNormalAttack.cs
+++ Assets/Scripts/MageNormalAttack.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class MageNormalAttack : MonoBehaviour {
 
     public float speed = 0.5f;
-
+    public float damage;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Debug.Log(col.gameObject.name);
+            col.gameObject.GetComponent<EnemyChar>().inflictDamage(3);
+            
+        }
+    }
 	// Update is called once per frame
 	void Update () {
 
